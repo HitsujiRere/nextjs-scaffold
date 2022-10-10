aae50a14b7ff91656dc68d7958ed49e6c0c0db66
HitsujiRere <hitsujirere@gmail.com>
2022-10-11 00:11:28 +0900

    Fix to load tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <p>Welcome to Next.js!</p>;
+  return <p className="text-2xl">Welcome to Next.js!</p>;
 };
 
 export default Home;
