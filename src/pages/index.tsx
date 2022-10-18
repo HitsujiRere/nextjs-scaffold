47b5fed037e84870182a3d2fb3d2dd3018b0c4da
HitsujiRere <hitsujirere@gmail.com>
2022-10-18 20:33:51 +0900

    Header, Layoutを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
-import { DefaultLayout } from 'src/layouts/DefaultLayout';
 
 const Home: NextPage = () => {
-  return (
-    <DefaultLayout>
-      <p className="text-xl">Welcome to Next.js!</p>
-      <Link href="/dog">
-        <a>Link to Dog</a>
-      </Link>
-    </DefaultLayout>
-  );
+  return <p className="text-xl">Welcome to Next.js!</p>;
 };
 
 export default Home;
