3f18c6ab8d365775e3ed5aa62e10dd715ac62167
HitsujiRere <hitsujirere@gmail.com>
2022-11-08 22:42:32 +0900

    Loading表示を追加、Headerへの追加

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,34 +1,55 @@
 'use client';
 
+import Image from 'next/image';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGoogle } from 'react-icons/fa';
 
 export default function AuthPage() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
-  if (session) {
+  if (status === 'loading') {
     return (
       <div>
-        <p>Name: {session.user?.name}</p>
-        <p>Email: {session.user?.email}</p>
-        <button className="btn" onClick={() => signOut()}>
-          Sign out
-        </button>
+        <h1 className="text-xl font-medium loading">Loading...</h1>
       </div>
     );
   }
 
-  return (
-    <div>
-      <p>Not signed in</p>
-      <div className="input-group">
-        <span>
-          <FaGoogle />
-        </span>
-        <button className="btn gap-2" onClick={() => signIn('google')}>
-          Sign in with Google
-        </button>
+  if (status === 'unauthenticated') {
+    return (
+      <div>
+        <h1 className="text-xl font-medium">Unauthenticated</h1>
+        <div className="input-group">
+          <span>
+            <FaGoogle />
+          </span>
+          <button className="btn" onClick={() => signIn('google')}>
+            Sign in with Google
+          </button>
+        </div>
+
+        <p>Not signed in</p>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if (status === 'authenticated') {
+    return (
+      <div>
+        <h1 className="text-xl font-medium">Authenticated</h1>
+        <button className="btn" onClick={() => signOut()}>
+          Sign out
+        </button>
+
+        <p>Name: {session.user?.name}</p>
+        <p>Email: {session.user?.email}</p>
+        <p>
+          <a className="link" href={session.user?.image ?? ''}>
+            {session.user?.image}
+          </a>
+        </p>
+        <Image src={session.user?.image ?? ''} alt="user image" width={96} height={96} />
+      </div>
+    );
+  }
 }
